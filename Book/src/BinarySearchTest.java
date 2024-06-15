596d12812a0b5429a45f52a1c9091ce1326ce139
UngSangYoon <yws804@gmail.com>
2024-06-15 23:20:11 +0900

    Fix naming to convention

--- Book/src/BinarySearchTest.java
+++ Book/src/BinarySearchTest.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class search_bsTest {
+class BinarySearchTest {
 	
 	BookManager mgr;
 	
